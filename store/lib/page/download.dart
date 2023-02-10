2793988b03916f831ac3e6109046808c057ce294
shitingbao <871258317@qq.com>
2023-02-10 15:41:20 +0800

    perf: linear progress

--- store/lib/page/download.dart
+++ store/lib/page/download.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
-class Download extends StatelessWidget {
+class Download extends StatefulWidget {
   const Download({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<Download> createState() => _DownloadState();
+}
+
+class _DownloadState extends State<Download> {
+  var txt = "下载";
+  var value = 0.6;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("data"));
+    return Center(
+        child: Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              value += 0.1;
+            });
+          },
+          child: Text(txt),
+        ),
+        Text("当前进度：" + value.toString()),
+        LinearProgressIndicator(
+          value: value,
+          backgroundColor: Colors.orange[200],
+          valueColor: AlwaysStoppedAnimation(Colors.red[300]),
+        ),
+        Container(
+          height: 20,
+        ),
+        CircularProgressIndicator(),
+      ],
+    ));
   }
 }
